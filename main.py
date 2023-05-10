13958d3137244ae2b436a863b756ac54f9db13d2
Safwan <jmackenziejr.8@gmail.com>
2023-05-10 01:22:23 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.title("Who will get married next?")
+st.subheader("Lets be realistic tho...")
+choices = ["Choose one...", "Yahya", "Abdullah BE", "Murad", "Abdullah SWE", "AbdurRahman GER", "Shamil", "Iznaur", "Habib", "Islam",
+              "AbdurRahman FRA", "Safwan", "Deni", "Abdullah GER", "AbdulMalik", "Muhammad", "Aydamir"]
+selection = st.selectbox("Niggas:", choices)
+
+if selection == choices[1]:
+    st.info("Perhaps")
+elif selection == choices[2]:
+    st.info("Always a strong choice")
+elif selection == choices[3]:
+    st.info("He is full of surprises")
+elif selection == choices[4]:
+    st.info("Strongest choice in sha Allah")
+elif selection == choices[5]:
+    st.info("Definitely a possibility")
+elif selection == choices[6]:
+    st.info("Sources say he is a specialist, so we don't know what he is truly capable of...")
+elif selection == choices[7]:
+    st.info("If he decides to put the red shoes back on none of us can compete with him")
+elif selection == choices[8]:
+    st.info("He probably already has 4 wives and hasn't told us")
+elif selection == choices[9]:
+    st.info("Tough times boxsh dac")
+elif selection == choices[10]:
+    st.info("Strong choice")
+elif selection == choices[11]:
+    st.info("Not likely")
+elif selection == choices[12]:
+    st.info("I don't think any of us would be surprised, and hopefully Yahya will invite us all to the second wedding")
+elif selection == choices[13]:
+    st.info("If he marries again before any of us do I don't think we would mentally recover from that)))")
+elif selection == choices[14]:
+    st.info("Another strong choice in sha Allah")
+elif selection == choices[15]:
+    st.info("Now that he has a license anything is possible. An object in motion stays in motion, but Edaev in motion is a force unknown to science.")
+elif selection == choices[16]:
+    st.info("Mans admitted that he has chosen burger over marriage so I don't need to say more)))")
